2d2203d34e9b3fc477fc0475827002fb03b65164
pdieguez4 <pdieguez4@iespompeufabra.cat>
2024-09-18 10:41:32 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
+ createCanvas(600,400);
  // put setup code here
 }
 
 function draw() {
+ background(127);
+ fill(255,223,196);
+ ellipse(300,200,220,300);
+ fill(81,209,246);
+ ellipse(250,150,50,30);
+ ellipse(350,150,50,30);
   // put drawing code here
 }
