350deb4cf736d4ec972f46ba2fc8773bafc83e0e
pdieguez4 <pdieguez4@iespompeufabra.cat>
2024-09-25 11:15:21 +0200

    Update sketch.js

--- ocell/sketch.js
+++ ocell/sketch.js
@@ -1,36 +1,36 @@
 function setup() {
- createCanvas(400,120)
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(204)
-  //Ocell esquerra
+  background(220);
   beginShape();
-  vertex(50,120); 
-  vertex(100,90);
-  vertex(110,60);
-  vertex(80,20);
-  vertex(210,60);
-  vertex(160,80);
-  vertex(200,90);
-  vertex(140,100);
-  vertex(130,120);
-  endShape();
-  fill(0);
-  ellipse(155,60,8,8);
-
-  //Ocell dreta
-  beginShape();
-  vertex(370,120); 
-  vertex(360,90);
-  vertex(290,60);
-  vertex(340,20);
-  vertex(280,60);
-  vertex(340,80);
-  vertex(280,90);
-  vertex(420,100);
-  vertex(460,120);
-  endShape();
-  fill(0);
-  ellipse(155,60,8,8);
+  vertex(20,50)
+  vertex(65,69)
+  vertex(70,65)
+  vertex(80,60)
+  vertex(85,61)
+  vertex(100,61)
+  vertex(95,63)
+  vertex(85,64)
+  vertex(84,68)
+  vertex(83,80)
+  vertex(81,90)
+  vertex(75,100)
+  vertex(65,105)
+  vertex(50,106)
+  vertex(40,105)
+  vertex(35,104)
+  vertex(30,102)
+  vertex(25,100)
+  vertex(20,97)
+  vertex(18,94)
+  vertex(24,96)
+  vertex(28,96)
+  vertex(35,94)
+  vertex(37,92)
+  vertex(38,91)
+  vertex(35,87)
+  
+  endShape(CLOSE);
 }
